cf1a9dee62f4f6760270b3fa2185774130070080
Bohdan <104660586+Bohdan100@users.noreply.github.com>
2023-07-24 16:09:02 +0300

    shopping

--- src/pages/ShoppingListPage/ShoppingListPage.tsx
+++ src/pages/ShoppingListPage/ShoppingListPage.tsx
@@ -1,32 +1,26 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import { FC } from 'react';
+import { IProduct } from 'types';
+import { useShoppingList } from 'hooks/useShoppingList';
 import { useTranslation } from 'react-i18next';
 
-import { selectProducts } from '../../redux/ShoppingList/shoppingListSelectors';
+import Loader from 'components/Loader';
 import MainContainer from 'components/MainContainer';
 import IngredientsShoppingList from 'components/IngredientsShoppingList';
-import ReusableTitle from '../../components/ReusableComponents/ReusableTitle';
-
-import { HeaderTable, StyledLoaderWrapper } from './ShoppingListPage.styled';
-import Loader from 'components/Loader';
+import ReusableTitle from 'components/ReusableComponents/ReusableTitle';
 import NotFoundWrapp from 'components/ReusableComponents/NotFoundWrapp';
 
-import {useShoppingList} from 'hooks/useShoppingList'
-import { IProduct } from 'types';
+import { HeaderTable, StyledLoaderWrapper } from './ShoppingListPage.styled';
 
 const isLoading = false;
 
-const ShoppingListPage = () => {
+const ShoppingListPage: FC = () => {
   const { t } = useTranslation();
 
- 
-  
   const shoppingList = useShoppingList().products;
-  console.log(shoppingList);
-  
 
-  const reverse = (arr: IProduct[]): IProduct[] => arr.map((_, index) => arr[arr.length - 1 - index]);
-  
+  const reverse = (arr: IProduct[]): IProduct[] =>
+    arr.map((_, index) => arr[arr.length - 1 - index]);
+
   const reversedShoppingList = reverse(shoppingList);
 
   return (
@@ -39,7 +33,7 @@ const ShoppingListPage = () => {
           <span>{t('shoppingListPage.remove')}</span>
         </p>
       </HeaderTable>
-      {isLoading ? ( 
+      {isLoading ? (
         <StyledLoaderWrapper>
           <Loader />
         </StyledLoaderWrapper>
